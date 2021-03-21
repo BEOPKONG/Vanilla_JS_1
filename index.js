9974eca4dc6ac66a0cb36ee7d1512816e0523f76
BEOPKONG <76099195+BEOPKONG@users.noreply.github.com>
2021-03-21 17:42:52 +0900

    +comments

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const greetings = document.querySelector(".js-greetings");
 
 
 
-function getTime() {
+function getTime() {  //clock
   const date = new Date();
   const hours = date.getHours();
   const minutes = date.getMinutes();
@@ -22,32 +22,34 @@ function saveName(name){
 }
 
 
-function askName(){
+function askName(){  //없다면 submit event를 감지해서 handleSubmit()으로
     form.addEventListener("submit", handleSubmit);
     console.log("askName");
 }
 
-function handleSubmit(event){
+function handleSubmit(event){ //submit되면 기본적으로 새로고침되는걸 막고 값을 저장
     event.preventDefault();
     const currentValue = input.value;
-    setItem(currentValue);
-    saveName(currentValue);
+    saveName(currentValue); //input에 사용자가 타이핑한 값을 저장
+    setItem();
+    
 
 }
 
 
 
-function setItem(userName) {
+function setItem() {  //있다면 localstrage에 있는값을 greetings에 뿌려준다
+    const userName = localStorage.getItem("user");
     greetings.innerText = `Hello ${userName}`;
     greetings.classList.add("showing");
     input.classList.add("disappear");
 }
 
 
-function checkItem() {
+function checkItem() {  //localstrange에 저장하기 예제
     const userName = localStorage.getItem("user");
     if(userName) { //있으면
-        setItem(userName);
+        setItem();
     } else { //없으면 
         askName();
         console.log("not");
